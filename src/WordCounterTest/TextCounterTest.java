65d41951739065830ab3425c6e6918ca9bbfa61e
Rickard Jansson <rickard.jansson@gmail.com>
2023-10-18 00:04:13 +0200

    Tweaked and added automated test cases

--- src/WordCounterTest/TextCounterTest.java
+++ src/WordCounterTest/TextCounterTest.java
@@ -7,22 +7,22 @@ public class TextCounterTest
 {
 
     @Test
-    public void testCharacterCount()
+    public void testCharacterCount() //Beräknat antal tecken inkluderar blanksteg och skiljetecken
     {
         TextCounter counter = new TextCounter();
-        counter.addLine("This is a test.");
-        counter.addLine("Another line.");
+        counter.addLine("This is a test line.");
+        counter.addLine("This is another test line!");
         int characterCount = counter.getCharacterCount();
-        Assertions.assertEquals(28, characterCount);
+        Assertions.assertEquals(46, characterCount);
     }
 
     @Test
-    public void testLineCount()
+    public void testLineCount() //Testar antal rader utan att användaren skriver stop.
     {
         TextCounter counter = new TextCounter();
-        counter.addLine("Line 1");
-        counter.addLine("Line 2");
-        counter.addLine("Line 3");
+        counter.addLine("Test line 1");
+        counter.addLine("Test line 2");
+        counter.addLine("Test line 3");
         int lineCount = counter.getLineCount();
         Assertions.assertEquals(3, lineCount);
     }
@@ -31,8 +31,8 @@ public class TextCounterTest
     public void testLineCountWithStop()
     {
         TextCounter counter = new TextCounter();
-        counter.addLine("Line 1");
-        counter.addLine("Line 2");
+        counter.addLine("Test line 1");
+        counter.addLine("Test line 2");
         counter.addLine("stop");
         int lineCount = counter.getLineCount();
         Assertions.assertEquals(2, lineCount);
@@ -55,11 +55,11 @@ public class TextCounterTest
         counter.addLine("This is a test.");
         counter.addLine("Longest word in this line is 'elephant'."); //Längsta ordet avslutas med punkt
         String longestWord = counter.getLongestWord();
-        Assertions.assertEquals("'elephant'", longestWord);
+        Assertions.assertEquals("'elephant'", longestWord); //Förväntat resultat, utan punkt
     }
 
     @Test
-    public void testUserTypedStop()
+    public void testUserTypedStopLowerCase() //Testar om boolean userTypedStop() deklareras 'false' med endast gemener
     {
         TextCounter counter = new TextCounter();
         counter.addLine("This is a test.");
@@ -67,5 +67,29 @@ public class TextCounterTest
         Assertions.assertFalse(counter.userTypedStop());
         counter.addLine("stop");
         Assertions.assertTrue(counter.userTypedStop());
+    }
+
+    @Test
+    public void testUserTypedStopUpperCase() //Testar om boolean userTypedStop() deklareras 'false' oavsett case sensitivity
+    {
+        TextCounter counter = new TextCounter();
+        counter.addLine("This is a test.");
+        counter.addLine("Another line.");
+        Assertions.assertFalse(counter.userTypedStop());
+        counter.addLine("Stop");
+        Assertions.assertTrue(counter.userTypedStop());
+    }
+
+    @Test
+    public void testUserTypedStopAndBlankSpace() //Testar om boolean userTypedStop() fortfarande är 'true' med blanksteg/utropstecken efter 'stop'
+    {
+        TextCounter counter = new TextCounter();
+        counter.addLine("This is a test.");
+        counter.addLine("Another line.");
+        Assertions.assertFalse(counter.userTypedStop());
+        counter.addLine("stop ");
+        Assertions.assertFalse(counter.userTypedStop());
+        counter.addLine("stop!");
+        Assertions.assertFalse(counter.userTypedStop());
     }
 }
